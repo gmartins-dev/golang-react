ddaf220db09875267587fd02132ee86dd6a61ab6
gmartins-dev <guilhermemm.dev@gmail.com>
2024-08-14 18:53:47 +0100

    chore: Ignore personal notes and add migrations for rooms and messages tables

---
+++ internal/store/pgstore/migrations/001_create_rooms_table.sql
@@ -0,0 +1,8 @@
+-- Write your migrate up statements here
+CREATE TABLE IF NOT EXISTS rooms (
+    "id"    uuid            PRIMARY KEY     NOT NULL    DEFAULT gen_random_uuid(),
+    "theme" VARCHAR(255)                    NOT NULL
+);
+
+---- create above / drop below ----
+DROP TABLE IF EXISTS rooms;
